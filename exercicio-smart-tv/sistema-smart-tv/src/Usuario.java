471606b17846edd79a0f805a42ffbeaeaaaf3920
José Sicuro <josesicurodev@gmail.com>
2024-05-03 12:02:15 -0300

    Primeiro Exercício - Smart Tv

---
+++ exercicio-smart-tv/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+       SmartTv smartTv = new SmartTv();
+
+       smartTv.diminuirVolume();
+       smartTv.diminuirVolume();
+       smartTv.diminuirVolume();
+       smartTv.aumentarVolume();
+
+       System.out.println("Canal Atual: " + smartTv.canal);
+
+       smartTv.mudarCanal(13);
+
+       System.out.println("Canal Atual: " + smartTv.canal);
+
+       System.out.println("Volume atual: " + smartTv.volume);
+
+       System.out.println("TV ligada? " + smartTv.ligada);
+       System.out.println("Canal Atual: " + smartTv.canal);
+       System.out.println("Volume atual: " + smartTv.volume);
+
+       smartTv.ligar();
+       System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+       smartTv.desligar();
+       System.out.println("TV ligada? " + smartTv.ligada);
+    }
+}

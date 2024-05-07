b1226d00bd29fa26fd93f61beaa13312f3bb2614
José Sicuro <josesicurodev@gmail.com>
2024-05-07 10:34:58 -0300

    Adicionado try para o scanner

--- desafio-dio-conta-banco/conta-banco/src/ContaTerminal.java
+++ desafio-dio-conta-banco/conta-banco/src/ContaTerminal.java
@@ -9,25 +9,25 @@ public class  ContaTerminal {
         //Obter pela classe Scanner os valores digitados no terminal
         //Exibir a mensagem da conta criada.
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            String numeroConta;
+            String numeroAgencia;
+            String nomeCompleto;
+            
+            double saldoConta;
 
-        String numeroConta;
-        String numeroAgencia;
-        String nomeCompleto;
-        
-        double saldoConta;
+            saldoConta = 237.48;
 
-        saldoConta = 237.48;
+            System.out.println("Por favor digite o número da agência! ");
+            numeroAgencia = scanner.next();
 
-        System.out.println("Por favor digite o número da agência! ");
-        numeroAgencia = scanner.next();
+            System.out.println("Por favor digite o número da sua conta!");
+            numeroConta = scanner.next();
 
-        System.out.println("Por favor digite o número da sua conta!");
-        numeroConta = scanner.next();
+            System.out.println("Agora, por favor, insira seu nome completo!");
+            nomeCompleto = scanner.next();
 
-        System.out.println("Agora, por favor, insira seu nome completo!");
-        nomeCompleto = scanner.next();
-
-        System.out.println("Olá, " + nomeCompleto + "! Obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo de " + saldoConta + " já está disponível para saque!" );
+            System.out.println("Olá, " + nomeCompleto + "! Obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo de " + saldoConta + " já está disponível para saque!" );
+        }
     }
 }
